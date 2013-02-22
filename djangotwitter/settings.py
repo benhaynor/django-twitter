01daa4a852551450400cb77a337bdd30431b48cd
benhaynor <ben@meltwater.org>
2013-02-22 13:48:58 +0000

    Templates working

--- djangotwitter/settings.py
+++ djangotwitter/settings.py
@@ -103,6 +103,7 @@ ROOT_URLCONF = 'djangotwitter.urls'
 WSGI_APPLICATION = 'djangotwitter.wsgi.application'
 
 TEMPLATE_DIRS = (
+    '/home/benhaynor/Documents/TechSemester2/Week8/djangotwitter/twitter/templates'
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
--- twitter/views.py
+++ twitter/views.py
@@ -1,4 +1,19 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.contrib import auth
 
-def say_hello(request):
-    return HttpResponse('Hello World!')
+def landing_page(request):
+    return render_to_response('landing_page.html')
+
+def login_view(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password=password)
+    if user is not None and user.is_active:
+        # Correct password, and the user is marked "active"
+        auth.login(request, user)
+        # Redirect to a success page.
+        return HttpResponseRedirect("/account/loggedin/")
+    else:
+        # Show an error page
+        return HttpResponseRedirect("/account/invalid/")
