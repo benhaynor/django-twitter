dad0626dcc34edd7c268047ac747bdf03c43fa2a
benhaynor <ben@meltwater.org>
2013-02-27 15:09:12 +0000

    about to delete things, make consistent with tutorial

--- twitter/polls.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from twitter.models import Tweet
-
-admin.site.register(Tweet)
-
--- twitter/urls.py
+++ twitter/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('twitter',
-    url(r'^$','views.landing_page'),
-    url(r'^logout$','views.logout')
+    url(r'^$','views.landing_page', name = 'home'),
+    url(r'^logout$','views.logout', name='logout')
 )
