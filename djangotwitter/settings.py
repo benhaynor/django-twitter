aba0b3c3276e2f2eb9d2e145c66f778c89a4e0a0
benhaynor <ben@meltwater.org>
2013-04-10 16:37:25 +0000

    No Change

--- djangotwitter/settings.py
+++ djangotwitter/settings.py
@@ -12,7 +12,6 @@ ADMINS = (
 MANAGERS = ADMINS
 
 if socket.gethostname() == 'bunbuntu':
-    #Local conviguration
     DATABASES = {
             'default': {
                 'ENGINE': 'django.db.backends.postgresql_psycopg2',#'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
