3a96d892b56f0aec405e344f9324bbe07dcf205a
benhaynor <ben@meltwater.org>
2013-04-16 13:40:02 +0000

    Removed rest_framework from installed apps

--- djangotwitter/settings.py
+++ djangotwitter/settings.py
@@ -148,7 +148,6 @@ INSTALLED_APPS = (
         'django.contrib.admin',
         # Uncomment the next line to enable admin documentation:
         'django.contrib.admindocs',
-        'rest_framework',
         'gunicorn',
         )
 
